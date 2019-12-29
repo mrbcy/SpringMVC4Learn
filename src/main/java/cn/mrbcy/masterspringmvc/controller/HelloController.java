d62514e1ce0db8e44f53ed48d80fa7516220e735
mrbcy <me@mrbcy.cn>
2019-12-29 23:37:55 +0800

    Hello Controller

--- src/main/java/cn/mrbcy/masterspringmvc/controller/HelloController.java
+++ src/main/java/cn/mrbcy/masterspringmvc/controller/HelloController.java
@@ -1,15 +1,17 @@
 package cn.mrbcy.masterspringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "Hello SpringMVC";
+    public String hello(@RequestParam(value = "name", defaultValue = "world")String userName, Model model) {
+        model.addAttribute("message", "Hello, " + userName);
+        return "resultPage";
     }
 }
